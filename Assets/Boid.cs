c11994051e54d65daa9be7cd2f017b95a0af80fe
Jahmaal-R <jahmaal.rivers@gmail.com>
2025-05-15 23:51:38 -0400

    First lab done!

--- Assets/Boid.cs
+++ Assets/Boid.cs
@@ -2,4 +2,30 @@ using UnityEngine;
 
 public class Boid : MonoBehaviour
 {
+    public Rigidbody rigidBody;
+
+    private void Awake ()
+    {
+        StartBoids();
+    }
+
+    void StartBoids()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        rigidBody.linearVelocity = Random.insideUnitSphere;
+        GetComponent<Renderer>().material.SetColor("_BaseColor", Random.ColorHSV(0, 1, 0.5f, 1, 0.5f, 1));
+    }
+
+    public void Update()
+    {
+        AlignToVelocity();
+    }
+
+    public void AlignToVelocity()
+    {
+        transform.forward = Vector3.RotateTowards(transform.forward, rigidBody.linearVelocity.normalized, Mathf.Deg2Rad * 1000 * Time.deltaTime, 100);
+    }
+
+
+
 }
--- Assets/BoidSimulation.cs
+++ Assets/BoidSimulation.cs
@@ -1,6 +1,62 @@
 using UnityEngine;
+using System.Collections.Generic;
+using static BoidSimulationControl;
 
 public class BoidSimulationControl : MonoBehaviour
 {
+    public GameObject boidPrefab;
+    public int boidsToSpawn = 10;
+     private List<Boid> boids;
 
+    public void Start()
+    {
+        BoidSpawn();
+    }
+
+    public enum ControlMode
+    {
+        Seek,
+        Pursue,
+        Food,
+        Obstacle
+    }
+
+    public ControlMode controlMode = ControlMode.Seek;
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            ControlMode controlMode = ControlMode.Seek;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            ControlMode controlMode = ControlMode.Pursue;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            ControlMode controlMode = ControlMode.Food;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            ControlMode controlMode = ControlMode.Obstacle;
+        }
+    }
+
+    private void BoidSpawn()
+    {
+        boids = new List<Boid>();
+
+        for (int i = 0; i < boidsToSpawn; i++)
+        {
+            GameObject newBoid = Instantiate(boidPrefab, new Vector3(Random.Range(-0.7f, 0.7f), Random.Range(-0.7f, 0.7f), Random.Range(-0.4f, 0.4f)), Random.rotation);
+            boids.Add(newBoid.GetComponent<Boid>());
+        }
+    }
 }
+
